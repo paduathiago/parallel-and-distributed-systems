3740074c3570de64293244841418b407651b8a70
Thiago Padua <paduathiago@outlook.com>
2023-09-12 21:50:14 -0300

    added resources struct and init_resources

--- ep1_main.c
+++ ep1_main.c
@@ -1,17 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <pthread.h>
+
+typedef struct
+{
+    int tid;
+    int free_time;
+    int critical_time;
+    int resources[8];
+    int num_resources;
+} Task;
+
+typedef struct
+{
+    bool available[8];
+} Resources;
+
+void init_resources(Resources *resources)
+{
+    for (int i = 0; i < 8; i++)
+        resources->available[i] = true;
+}
 
 int main()
 {
     int tid, free_time, critical_time;
-    int resources[8];
-    int num_resources = 0;
+    int requested_resources[8];
+    Resources *resources;
+    
+    init_resources(resources);
     
     while(scanf("%d %d %d", tid, free_time, critical_time) == 3)  // Get input as long as there are 3 integers in the line
     {
+
         // Process the first part of the input
         
-        while (num_resources < 8 && scanf("%d", &resources[num_resources]) == 1) 
+        int num_resources = 0;
+        
+        while (num_resources < 8 && scanf("%d", &requested_resources[num_resources]) == 1) 
             num_resources++;
         
         // Process the second part of the input (requested resources)
