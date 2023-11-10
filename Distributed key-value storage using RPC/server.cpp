c46d6b8329d36090d4246a48ed914f20f0ce4cbb
Thiago Padua <paduathiago@outlook.com>
2023-11-10 15:17:54 -0300

    first version of server

---
+++ Distributed key-value storage using RPC/server.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <unordered_map>
+#include <grpcpp/grpcpp.h>
+#include "storage.pb.h"
+#include "storage.grpc.pb.h"
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+using std::cout;
+using std::endl;
+using std::string;
+
+class KeyValueServiceImpl final : public KeyValueService::Service {
+private: 
+    std::unordered_map<int, std::string> dict;
+
+public:
+    Status insert(ServerContext* context, const KeyValue* request, InsertResponse* response) override {
+        // Lógica de inserção aqui
+        dict.insert({request->key(), request->value()});
+        cout << "Inserindo: " << request->key() << ", " << request->value() << endl;
+        response->set_success(true);
+        return Status::OK;
+    }
+
+    Status get(ServerContext* context, const Key* request, Value* response) override {
+        // Lógica de consulta aqui
+        cout << "Consultando chave: " << request->key() << endl;
+        if (dict.find(request->key) != dict.end()) 
+            response->set_value(dict[request->key]);
+    
+        else 
+            // A chave não existe
+            response->set_value("");
+
+        return Status::OK;
+    }
+    /*Status activate(ServerContext* context, const Key* request, Value* response) override {
+        // Lógica de consulta aqui
+        cout << "Ativando chave: " << request->key() << endl;
+        response->set_value("Valor correspondente à chave");
+        return Status::OK;
+    }*/
+};
+
+void RunServer() {
+    string server_address("0.0.0.0:50051");
+    KeyValueServiceImpl service;
+
+    ServerBuilder builder;
+    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+    builder.RegisterService(&service);
+
+    std::unique_ptr<Server> server(builder.BuildAndStart());
+    cout << "Servidor gRPC aguardando em " << server_address << endl;
+    server->Wait();
+}
+
+int main() {
+    RunServer();
+    return 0;
+}
