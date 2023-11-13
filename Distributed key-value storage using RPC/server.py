378fbc33f0f325d88c913c5a3b932949d6a4cd83
paduathiago <paduathiago@outlook.com>
2023-11-13 16:41:13 -0300

    addded event and completed function to terminate server

--- Distributed key-value storage using RPC/server.py
+++ Distributed key-value storage using RPC/server.py
@@ -1,12 +1,14 @@
 import grpc
 import sys
+import threading
 from concurrent import futures
 
 import pairs_pb2, pairs_pb2_grpc
 
 class Pair(pairs_pb2_grpc.PairsServicer):
-    def __init__(self):
+    def __init__(self, event):
         self.my_dict = {}
+        self.stop_event = event
     
     def insert(self, context, request):
         if request.key not in self.my_dict:
@@ -19,16 +21,22 @@ class Pair(pairs_pb2_grpc.PairsServicer):
         return pairs_pb2.Value(value=self.my_dict.get(request.key, ""))
     
     def activate(self, request, context):
-        pass
+        if len(sys.argv) > 1:
+            # TODO: activate server
+            pass
+        return pairs_pb2.SuccResponse(success=0)
     
     def terminate(self, context, request):
-        # TODO: terminate server
+        self.stop_event.set()
         return pairs_pb2.SuccResponse(success=0)
 
 
 def serve():
+    stop_event = threading.Event()
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    pairs_pb2_grpc.add_PairsServicer_to_server(Pair(), server)
+    pairs_pb2_grpc.add_PairsServicer_to_server(Pair(stop_event), server)
     server.add_insecure_port('localhost:8888')
     server.start()
-    server.wait_for_termination()
+    stop_event.wait()
+    server.stop(1)
+    # server.wait_for_termination()
