45a522ffdd8ae2382cc0cdb55b349074e7c92f0c
paduathiago <paduathiago@outlook.com>
2023-11-13 16:41:40 -0300

    added central server

---
+++ Distributed key-value storage using RPC/central_server.py
@@ -0,0 +1,12 @@
+import socket
+import grpc
+
+class CentralServer:
+    def __init__(self):
+        self.my_servers = {}
+    
+    def register(self, request, context):
+        pass
+
+    def terminate(self, request, context):
+        pass
