bf1b03fdbe3911359cc46ee038be74a96d1e049f
Thiago Padua <paduathiago@outlook.com>
2023-11-14 23:45:37 -0300

    added terminate method and serve function

--- Distributed key-value storage using RPC/central_server.py
+++ Distributed key-value storage using RPC/central_server.py
@@ -1,15 +1,38 @@
 import socket
+import threading
+from concurrent import futures
 import grpc
 
 import pairs_pb2, pairs_pb2_grpc
 
 class CentralServer:
-    def __init__(self):
+    def __init__(self, event):
         self.my_servers = {}
+        self.stop_event = event
     
+
     def register(self, request, context):
         self.my_server.add(request.server_id, request.server_keys)
         return pairs_pb2.KeyCounter(count=len(self.my_servers[request.server_id]))
     
+
     def terminate(self, request, context):
+        self.stop_event.set()
+        return pairs_pb2.SuccResponse(success=0)
+    
+
+    def findOwner(self, request, context):
         pass
+
+
+def serve():
+    stop_event = threading.Event()
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    pairs_pb2_grpc.add_CentralServerServicer_to_server(CentralServer(stop_event), server)
+    server.add_insecure_port('localhost:8888')
+    server.start()
+    stop_event.wait()
+    server.stop(1)
+
+if __name__ == '__main__':
+    serve()
