c31548b23bd383208b15bd01a9ac01de275883d0
Thiago Padua <paduathiago@outlook.com>
2023-11-16 12:06:20 -0300

    changed the logic of the central server: dictionary now takes integers as keys and id strings as values

--- Distributed key-value storage using RPC/central_server.py
+++ Distributed key-value storage using RPC/central_server.py
@@ -1,30 +1,35 @@
-import socket
-import threading
-from concurrent import futures
 import grpc
+import socket
+import sys
+import threading
+
+from concurrent import futures
 
 import pairs_pb2, pairs_pb2_grpc
 
 class CentralServer:
     def __init__(self, event):
-        self.my_servers = {}
+        self.keys_servers = {}
         self.stop_event = event
     
 
     def register(self, request, context):
-        self.my_server.add(request.server_id, request.server_keys)
-        return pairs_pb2.KeyCounter(count=len(self.my_servers[request.server_id]))
-    
+        keys_processed = 0
+        for key in request.server_keys:
+            self.keys_servers[key] = request.server_id
+            keys_processed += 1
+        return pairs_pb2.KeyCounter(count=keys_processed)
+
 
     def terminate(self, request, context):
         self.stop_event.set()
         return pairs_pb2.SuccResponse(success=0)
-    
+
 
     def findOwner(self, request, context):
-        for server_id in self.my_servers:
-            if request.key in self.my_servers[server_id]:
-                return pairs_pb2.ServerId(id=server_id)  # Check if it is possible to have a server without the keys list
+        for key in self.keys_servers:
+            if key == request.key:
+                return pairs_pb2.ServerId(id=self.keys_servers[key])
         return pairs_pb2.ServerId(id="")
 
 
@@ -32,7 +37,7 @@ def serve():
     stop_event = threading.Event()
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     pairs_pb2_grpc.add_CentralServerServicer_to_server(CentralServer(stop_event), server)
-    server.add_insecure_port('localhost:8888')
+    server.add_insecure_port(f"[::]:{sys.argv[1]}")
     server.start()
     stop_event.wait()
     server.stop(1)
